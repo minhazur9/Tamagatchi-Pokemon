51e27af83ac3bfe5fa7c259560d8b6e2fde6968b
min <minhazur9@gmail.com>
2020-09-22 21:51:13 -0400

    egg now disappears, screen flashes, and the chosen pokemon appears

--- scripts/app.js
+++ scripts/app.js
@@ -12,29 +12,49 @@
 
 //-----------------Event Listeners---------------------
 
-$('#screen').on('click',function(event){
-    $('#start').css({"animation-name":"fadeOut", 
-                     "animation-duration": "200ms"
-                    });
+$('#screen').on('click', function (event) {
+    $('#start').css({
+        "animation-name": "fadeOut",
+        "animation-duration": "200ms"
+    });
     $('#screen').append('<img src="./images/sun.png" alt="The Sun" id="sun"></img>');
     $('#pokemon-list').append('<li><img src="./images/bulbasaur.png" alt="Bulbsaur" id="bulbasaur"></img></li>');
     $('#pokemon-list').append('<li><img src="./images/squirtle.png" alt="Squirtle" id="squirtle"></img></li>');
     $('#pokemon-list').append('<li><img src="./images/charmander.png" alt="Chamander" id="charmander"></img></li>');
-    $('#pokemon-list').css({"animation-name":"drop",
-                            "animation-duration": "1.7s",
-                            "animation-delay": "1s"
-                           });
+    $('#pokemon-list').css({ "animation-name": "drop" });
     $('#screen').off('click');
 })
 
-$('#pokemon-list').on('click', 'li', function(event){
-    $('#pokemon-list li').css({"animation-name":"fadeOut", 
-                               "animation-duration": "200ms", 
-                               "animation-fill-mode":"forwards"
-                              });
-    $('#egg').css({"animation-name":"shake",
-                    "animation-duration":"2s",
-                    "animation-fill-mode":"forwards"
-                  })
+$('#pokemon-list').on('click', 'li', function (event) {
+    let t = 0;
+    $('#start').remove();
+    $('#pokemon-list li').css({ "animation-name": "fadeOut" });
+    $('#screen').css({ "animation-name": "screen-fade" })
+    $("#egg").css({ "animation-name": "shake" });
+    animate = setInterval(function () {
+        t++;
+        console.log(t);
+        if (t === 3) {
+            $('#egg').css({
+                "animation-name": "fadeOut",
+                "animation-duration": "200ms",
+            })
+        }
+        if (t === 4) {
+            $('.pet').detach().appendTo('#screen');
+        }
+        if (t === 5) {
+            $('#egg').remove();
+            clearInterval(animate);
+        }
+
+        if (t === 6) {
+        }
+    }, 1000)
+
+});
+
+$('#pokemon-list').on('click', 'li', function () {
+    $(this).addClass('pet');
 })
 
