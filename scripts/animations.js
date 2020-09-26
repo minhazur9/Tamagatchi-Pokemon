782eea3f56ab103808b0211cdd8ac56bae538810
min <minhazur9@gmail.com>
2020-09-26 11:47:01 -0400

    Fixed error when trying to get data from food that doesn't exist yet

--- scripts/animations.js
+++ scripts/animations.js
@@ -19,19 +19,12 @@ function hatchEgg() {
 // The first evolution
 function midEvolve() {
     if ($('#charmander').is('img')) {
-        $($pet).detach();
+        $($tag).detach();
         $('#charmander').remove();
         $('#screen').append('<img src="./images/charmeleon.png" alt="Charmeleon" id="charmeleon" class="pet">');
         $pet = $('#charmeleon');
         $($pet).append($tag);
-        if (speed > 0) {
-            $pet.css({ 'transform': `scaleX(-1)` });
-            $tag.css({ 'transform': `scaleX(-1)` });
-        }
-        else {
-            $pet.css({ 'transform': `scaleX(1)` });
-            $tag.css({ 'transform': `scaleX(1)` });
-        }
+        
     }
 }
 
@@ -43,14 +36,6 @@ function finalEvolve() {
         $('#screen').append('<img src="./images/charizard.png" alt="Charizard" id="charizard" class="pet">');
         $pet = $('#charizard');
         $($pet).append($tag);
-        if (speed > 0) {
-            $pet.css({ 'transform': `scaleX(-1)` });
-            $tag.css({ 'transform': `scaleX(-1)` });
-        }
-        else {
-            $pet.css({ 'transform': `scaleX(1)` });
-            $tag.css({ 'transform': `scaleX(1)` });
-        }
     }
 }
 
@@ -60,24 +45,20 @@ function move() {
         $pet
             .velocity({ left: `${movementX += speed}px`, top: `${movementY -= 15}px` }, { duration: 500 })
             .velocity({ left: `${movementX += speed}px`, top: `${movementY += 15}px` }, { duration: 500 });
-        if (movementX < 30) {
-            setTimeout(function () {
-                $pet.css({ 'transform': `scaleX(-1)` });
-                $tag.css({ 'transform': `scaleX(-1)` });
-            }, 1500)
+        if (movementX < 30 || movementX > 1060) {
 
             speed *= -1;
         }
-        else if (movementX > 1060) {
-            setTimeout(function () {
-                $pet.css({ 'transform': `scaleX(1)` });
-                $tag.css({ 'transform': `scaleX(1)` });
-            }, 1500)
-            speed *= -1;
-
+        if (speed > 0) {
+            $pet.css({ 'transform': `scaleX(-1)` });
+            $tag.css({ 'transform': `scaleX(-1)` });
+        }
+        else {
+            $pet.css({ 'transform': `scaleX(1)` });
+            $tag.css({ 'transform': `scaleX(1)` });
         }
 
-        if ($('img').hasClass('.rare_candy')){
+        if ($('img').hasClass('rare_candy')){
             if ($($pet).offset().left >= $('.rare_candy').offset().left - 20 && $($pet).offset().left <= $('.rare_candy').offset().left + 20) {
                 $('.rare_candy').remove();
                 myPet.hungerDown();
